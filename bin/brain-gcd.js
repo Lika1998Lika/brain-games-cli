3ac528da0db317f379d5b63d007b02488a303384
Anzhelika <anzhelika_vanuyto@mail.ru>
2023-05-13 22:26:52 +0300

    add brain-gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,25 +1,18 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import game from "../src/index.js";
 
 const getRandomArbitrary = (min, max) =>
-
-Math.floor(Math.random() * (max - min) + min):
+  Math.floor(Math.random() * (max - min) + min);
 
 const gcdTwoNumbers = (a, b) => (!b ? a : gcdTwoNumbers(b, a % b));
 
+const rules = "Find the greatest common divisor of given numbers.";
 const brainGcd = () => {
-  console.log("Welcome to the Brain Games!");
-  const name = readlineSync.question("May I have your name? ");
-  console.log(`Hello, ${name}`);
-  console.log("Find the greatest common divisor of given numbers.");
+  const number1 = getRandomArbitrary(1, 100);
+  const number2 = getRandomArbitrary(1, 100);
+  const question = `${number1} ${number2}`;
+  const correctAnswer = gcdTwoNumbers(number1, number2);
+  return [question, String(correctAnswer)];
 };
 
-for (let i = 0; i < 3; i += 1) {
-  const a = getRandomArbitrary(1, 100);
-  const b = getRandomArbitrary(1, 100);
-  console.log(`Question: ${a} ${b}`);
-  const userAnswer = readlineSync.question("Your answer: ");
-  
-}
-
-brainGcd();
+game(brainGcd, rules);
