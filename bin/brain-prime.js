9e35b7b000cefaca318887ec2982acdeced4e1d6
Anzhelika <anzhelika_vanuyto@mail.ru>
2023-05-14 00:01:06 +0300

    add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import game from '../src/index.js';
+
+const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const minValue = 1;
+const maxValue = 101;
+const isPrime = (num) => {
+  for (let i = 2; i * i <= num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+const brainPrime = () => {
+  const question = getRandomArbitrary(minValue, maxValue);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+}
+
+game(brainPrime, rules);
